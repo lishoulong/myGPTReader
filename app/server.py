4d0d19eab7918a1544cf6cbd175677e2951a2bc9
lishoulong <lishoulong@bytedance.com>
2023-04-22 19:53:55 +0800

    feat: log load

--- app/server.py
+++ app/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.8
 import requests
+import json
 from event import EventManager
 from flask import Flask, jsonify, request
 from flask_apscheduler import APScheduler
@@ -49,6 +50,8 @@ def msg_error_handler(ex):
 @app.route("/api-endpoint", methods=["POST"])
 def callback_event_handler():
     # init callback instance and handle
+    dict_data = json.loads(request.data)
+    print(f"api-endpoint dict_data->{dict_data}")
     logger.info('=====> api-endpoint !')
     event_handler, event = event_manager.get_handler_with_event(VERIFICATION_TOKEN, ENCRYPT_KEY)
     if event_handler is None:
