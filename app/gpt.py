be77045267339ceb0a5e24d8e05f08be2c850126
Fred lee <2716648627@qq.com>
2023-05-01 17:36:32 +0800

    Feat/bytem (#39)
    * Update fly.toml image version
    
    * feat: 增加 chatbot 根据 parent_thread_id 单例逻辑
    
    ---------
    
    Co-authored-by: GitHub Actions <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: lishoulong <lishoulong@bytedance.com>

--- app/gpt.py
+++ app/gpt.py
@@ -12,77 +12,38 @@ from utils.chat_parent_id import write_parent_id, find_key_by_value, get_info_fr
 from utils.fetch_web import get_urls, get_text_from_urls
 from config import OPENAI_API_KEY, ACCESS_TOKEN
 
-# , base_url="https://openabc.online/backend-api/"
 logging = setup_logger('my_gpt_reader_gpt')
-# use chatgpt web to answer
-chatbot = Chatbot(config={
-    "access_token": ACCESS_TOKEN,
-    "paid": True,
-}, conversation_id=None, parent_id=None, session_client=None, lazy_loading=False)
-# SPEECH_KEY = os.environ.get('SPEECH_KEY')
-# SPEECH_REGION = os.environ.get('SPEECH_REGION')
-# 将 API 密钥放入列表中
-# api_keys = [OPENAI_API_KEY, OPENAI_API_KEY_SECOND, OPENAI_API_KEY_THIRD]
-# # 随机选择一个 API 密钥
-# chosen_api_key = random.choice(api_keys)
-# 将选定的 API 密钥分配给 openai.api_key
 openai.api_key = OPENAI_API_KEY
 print(f'OPENAI_API_KEY =>> {OPENAI_API_KEY}')
+# use chatgpt web to answer
+chatbot = {}
+obj = {}
 
 
 def get_answer_from_web(dialog_messages, parent_thread_id, thread_id):
     logging.info(
         f'=====> Use chatGPT WEB to answer! conversation_id={thread_id}, parent_id={parent_thread_id}, if equal = {thread_id == parent_thread_id}')
-    parent_id = None
+    chatbot = get_chatbot(parent_thread_id, thread_id)
     # thread_id == parent_thread_id 表示飞书新启动一个消息，那么这时候需要把 chatgpt 的 parent_id 清空或者重新获取历史的对应关系
-    if chatbot.parent_id is None or thread_id == parent_thread_id:
-        logging.info(
-            f'=====> chatbot.parent_id is None or thread_id == parent_thread_id '
-            f'parent_id={parent_thread_id}, if equal = {thread_id == parent_thread_id}'
-        )
-        parent_id = get_parent_id(parent_thread_id)
+    parent_id = get_parent_id(
+        parent_thread_id) if chatbot.parent_id is None or thread_id == parent_thread_id else None
     response = ""
     logging.info(
         f"conversation_mapping-{chatbot.conversation_mapping}, conversation_id - {chatbot.conversation_id}, parent_id - {parent_id}")
-    if chatbot.conversation_id is None and parent_id is not None:
-        # 如果 PARENT_CHILD_MAPPING_FILE 不存在就走创建写入逻辑
-        if not os.path.exists(PARENT_CHILD_MAPPING_FILE):
-            chatbot._Chatbot__map_conversations()
-            # 把对话信息写入文件系统
-            write_parent_child_id(chatbot.conversation_mapping)
-        else:
-            # 提取文件内容
-            chatbot.conversation_mapping = get_info_from_file()
-        logging.info(
-            f"conversation_mapping1111- conversation_mapping {chatbot.conversation_mapping }")
-        chatbot.conversation_id = find_key_by_value(
-            parent_id, chatbot.conversation_mapping)
-        logging.info(
-            f"conversation_mapping222- conversation_id {chatbot.conversation_id }")
+    # 适用于重启了程序，在之前的历史记录上重新回复消息，根据飞书的 parent_thread_id 获取到了 chatgpt 的 parent_id
+    update_chatbot_conversation_mapping(chatbot, parent_id)
     # 如果出现 gpt4 限流的问题，需要降级到 gpt-3.5-turbo
     model = ""
     try:
         model = "gpt-4"
-        for data in chatbot.ask(
-            prompt=dialog_messages,
-            conversation_id=chatbot.conversation_id,
-            parent_id=parent_id,
-            model=model
-        ):
-            response = data["message"]
+        response = ask_chatbot(chatbot, dialog_messages, model, parent_id)
         print(f"get_answer_from_chatGPT => {response}")
     except revChatGPT.typings.Error as e:
         # 429错误处理
         if "model_cap_exceeded" in str(e):
             # 降级到较低版本的API
             model = "gpt-3.5-turbo"
-            for data in chatbot.ask(
-                prompt=dialog_messages,
-                conversation_id=chatbot.conversation_id,
-                parent_id=chatbot.parent_id,
-                model=model
-            ):
-                response = data["message"]
+            response = ask_chatbot(chatbot, dialog_messages, model, parent_id)
             print(
                 f"get_answer_from_chatGPT (fallback to gpt-3.5-turbo) => {response}")
         else:
@@ -169,3 +130,46 @@ def get_text_from_path(path: str) -> str:
             text += page.extract_text()
         documents.append(text)
     return documents
+
+
+def get_chatbot(parent_thread_id, thread_id):
+    # 飞书重新启动一个消息，就需要相应的重新初始化 chatbot，需要维护 parent_thread_id 和 chatbot 的对应关系
+    if thread_id == parent_thread_id:
+        chatbot = Chatbot(config={
+            "access_token": ACCESS_TOKEN,
+            "paid": True,
+        }, conversation_id=None, parent_id=None, session_client=None, lazy_loading=False)
+        obj[parent_thread_id] = chatbot
+    else:
+        chatbot = obj[parent_thread_id]
+    return chatbot
+
+
+def update_chatbot_conversation_mapping(chatbot, parent_id):
+    if chatbot.conversation_id is None and parent_id is not None:
+        # 如果 PARENT_CHILD_MAPPING_FILE 不存在就走创建写入逻辑
+        if not os.path.exists(PARENT_CHILD_MAPPING_FILE):
+            chatbot._Chatbot__map_conversations()
+            # 把对话信息写入文件系统
+            write_parent_child_id(chatbot.conversation_mapping)
+        else:
+            # 提取文件内容
+            chatbot.conversation_mapping = get_info_from_file()
+        logging.info(
+            f"conversation_mapping1111- conversation_mapping {chatbot.conversation_mapping }")
+        chatbot.conversation_id = find_key_by_value(
+            parent_id, chatbot.conversation_mapping)
+        logging.info(
+            f"conversation_mapping222- conversation_id {chatbot.conversation_id }")
+
+
+def ask_chatbot(chatbot, dialog_messages, model, parent_id):
+    response = ""
+    for data in chatbot.ask(
+        prompt=dialog_messages,
+        conversation_id=chatbot.conversation_id,
+        parent_id=parent_id,
+        model=model
+    ):
+        response = data["message"]
+    return response
